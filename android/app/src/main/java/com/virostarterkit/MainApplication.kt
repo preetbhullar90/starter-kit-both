006f3b50568383f96c259fb54885b194eb432662
gurpreetbhullar70 <gurpreet.singhb90@gmail.com>
2024-02-16 11:13:39 +0000

    change Anroid config for working both devices Ios and Andriod

--- android/app/src/main/java/com/virostarterkit/MainApplication.kt
+++ android/app/src/main/java/com/virostarterkit/MainApplication.kt
@@ -11,35 +11,40 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.viromedia.bridge.ReactViroPackage;
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : DefaultReactNativeHost(this) {
-        override fun getPackages(): List<ReactPackage> =
-            PackageList(this).packages.apply {
-              // Packages that cannot be autolinked yet can be added manually here, for example:
-              // add(MyReactNativePackage())
+    override val reactNativeHost: ReactNativeHost =
+            object : DefaultReactNativeHost(this) {
+
+
+
+                override fun getPackages(): List<ReactPackage> =
+                        PackageList(this).packages.apply {
+                            // Packages that cannot be autolinked yet can be added manually here, for example:
+                            // add(MyReactNativePackage())
+                            add(ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("AR")));
+                        }
+
+                override fun getJSMainModuleName(): String = "index"
+
+                override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+                override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+                override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
             }
 
-        override fun getJSMainModuleName(): String = "index"
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
 
-        override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-        override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-        override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }
-
-  override val reactHost: ReactHost
-    get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, false)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
-      load()
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            // If you opted-in for the New Architecture, we load the native entry point for this app.
+            load()
+        }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
-    ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
-  }
 }
